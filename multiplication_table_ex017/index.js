4f7b9149624bd259c20d2378606e5e7b2b79808f
Victor_estudos_2023 <canalampmoficial@gmail.com>
2023-11-01 22:44:58 -0300

    update Practices4

--- multiplication_table_ex017/index.js
+++ multiplication_table_ex017/index.js
@@ -1,1 +1,20 @@
-alert('ola')
+function table() {
+    let num = document.getElementById('txtn');
+    let tab = document.getElementById('seltab');
+
+    if (num.value.length == 0) {
+        window.alert('[ERROR!] Type a valid Number!');
+    } else {
+        let n = Number(num.value);
+        let c = 1;
+        tab.innerHTML = '';
+        while (c <= 10){
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n*c}`;
+            item.value = `tab${c}`;
+            tab.appendChild(item);
+            c++;
+        }
+    }
+
+}
