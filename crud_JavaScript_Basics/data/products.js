e136096a44974a9664ae74c8a46ba0b9d791ec24
Victor_estudos_2023 <canalampmoficial@gmail.com>
2024-01-08 09:26:33 -0300

    commit

---
+++ crud_JavaScript_Basics/data/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        id: 1,
+        model: "Air Max 90",
+        color: "White/Red",
+        price: 199.99,
+        sizesAvailable: [39, 40, 41, 42, 43],
+        quantity: 10
+    },
+    {
+        id: 2,
+        model: "Air Force 1",
+        color: "Black",
+        price: 149.99,
+        sizesAvailable: [38, 39, 40, 41, 42],
+        quantity: 15
+    },
+    {
+        id: 3,
+        model: "React Element 55",
+        color: "Navy Blue",
+        price: 179.99,
+        sizesAvailable: [40, 41, 42, 43, 44],
+        quantity: 8
+    } 
+]
+
+modele.exports = products;
---
+++ crud_JavaScript_Basics/data/users.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        id: 1,
+        name: "Victor Silva",
+        email: "canalampm@gmail.com",
+        password: "123456abc"
+    },
+    {
+        id: 2,
+        name: "Fabiana Bauer",
+        email: "fabiana@gmail.com",
+        password: "123456abc"
+    },
+    {
+        id: 3,
+        name: "Jack Jackson",
+        email: "Jack@gmail.com",
+        password: "3223abc"
+    },
+    {
+        id: 4,
+        name: "Pedro Salt",
+        email: "saltpedro@gmail.com",
+        password: "vcs456abc"
+    },
+    {
+        id: 5,
+        name: "Coffee Salt",
+        email: "saltpedro@gmail.com",
+        password: "vcs456abc"
+    }
+]
+
+module.exports = users;
